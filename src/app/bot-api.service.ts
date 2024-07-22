86cfb8222d1a8df20b5dcd65a94ddcd70428a480
Paweł Pacanowski <pawelpacanowski.13@gmail.com>
2024-07-22 14:32:44 +0200

    Updated api

--- src/app/bot-api.service.ts
+++ src/app/bot-api.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import {HttpClient, HttpErrorResponse, HttpParams} from '@angular/common/http';
 import {map, Observable, throwError} from 'rxjs';
 
 @Injectable({
@@ -11,55 +11,99 @@ export class BotApiService {
   constructor(private http: HttpClient) {}
 
   checkIfServerExists(serverID: string): Observable<boolean> {
-    let url: string = `${this.api}/servers/check_if_exists/${serverID}`;
-    return this.http.get<boolean>(url);
+    let url: string = `${this.api}/servers/check_if_exists`;
+    let params = new HttpParams().set('dc_server_id', serverID);
+
+    return this.http.get<{ exists: boolean }>(url, { params }).pipe(
+      map(response => response.exists)
+    );
   }
 
   getServerTotalWords(serverID: string): Observable<string> {
-    let url: string = `${this.api}/servers/get_total_words/${serverID}`;
-    return this.http.get(url, { responseType: 'text' });
+    let url: string = `${this.api}/servers/get_total_words`;
+    let params = new HttpParams().set('dc_server_id', serverID);
+
+    return this.http.get<{ total_words: number }>(url, { params }).pipe(
+      map(response => response.total_words.toString())
+    );
   }
 
   getServerTotalFlaggedWords(serverID: string): Observable<string> {
-    let url: string = `${this.api}/servers/get_total_flagged_words/${serverID}`;
-    return this.http.get(url, { responseType: 'text' });
+    let url: string = `${this.api}/servers/get_total_flagged_words`;
+    let params = new HttpParams().set('dc_server_id', serverID);
+
+    return this.http.get<{ total_flagged_words: number }>(url, { params }).pipe(
+      map(response => response.total_flagged_words.toString())
+    );
   }
 
   getServerWords(serverID: string): Observable<any> {
-    let url: string = `${this.api}/servers/get_flagged_words_count/${serverID}`;
-    return this.http.get(url);
+    let url: string = `${this.api}/servers/get_flagged_words`;
+    let params = new HttpParams().set('dc_server_id', serverID);
+
+    return this.http.get<{ words: object }>(url, { params }).pipe(
+      map(response => response.words)
+    );
   }
 
   deleteWord(serverID: string, word: string) {
-    let url: string = `${this.api}/servers/remove_flagged_words/${serverID}`;
-    let data = [word];
-    return this.http.delete(url, {body: data});
+    let url: string = `${this.api}/servers/unflag_words`;
+    let params = new HttpParams().set('dc_server_id', serverID);
+
+    return this.http.patch(url, [word], {params}).pipe(
+      map(response => response)
+    );
   }
 
   addWord(serverID: string, word: string) {
-    let url: string = `${this.api}/servers/add_flagged_words/${serverID}`;
-    let data = [word];
-    return this.http.put(url, data);
+    let url: string = `${this.api}/servers/flag_words`;
+    let params = new HttpParams().set('dc_server_id', serverID);
+
+    return this.http.patch(url, [word], { params }).pipe(
+      map(response => response)
+    );
   }
 
   getAllMembers(serverID: string): Observable<string[]> {
-    let url: string = `${this.api}/servers/get_all_members/${serverID}`;
-    return this.http.get<string[]>(url);
+    let url: string = `${this.api}/servers/get_members_ids`;
+    let params = new HttpParams().set('dc_server_id', serverID);
+
+    return this.http.get<{ ids: string[] }>(url, { params }).pipe(
+      map(response => response.ids)
+    );
   }
 
   getMemberTotalFlaggedWords(serverID: string, userID: string): Observable<string> {
-    let url: string = `${this.api}/users/get_total_flagged_words/${serverID}/${userID}`;
-    return this.http.get(url, { responseType: 'text' });
+    let url: string = `${this.api}/users/get_total_flagged_words`;
+    let params = new HttpParams()
+      .set('dc_server_id', serverID)
+      .set('dc_user_id', userID)
+
+    return this.http.get<{ total_flagged_words: number }>(url, { params }).pipe(
+      map(response => response.total_flagged_words.toString())
+    );
   }
 
   getMemberTotalWords(serverID: string, userID: string): Observable<string> {
-    let url: string = `${this.api}/users/get_total_words/${serverID}/${userID}`;
-    return this.http.get(url, { responseType: 'text' });
+    let url: string = `${this.api}/users/get_total_words`;
+    let params = new HttpParams()
+      .set('dc_server_id', serverID)
+      .set('dc_user_id', userID)
+
+    return this.http.get<{ total_words: number }>(url, { params }).pipe(
+      map(response => response.total_words.toString())
+    );
   }
 
   getMemberWords(serverID: string, userID: string): Observable<any> {
-    let url: string = `${this.api}/users/get_flagged_words_count/${serverID}/${userID}`;
-    return this.http.get(url);
+    let url: string = `${this.api}/users/get_flagged_words`;
+    let params = new HttpParams()
+      .set('dc_server_id', serverID)
+      .set('dc_user_id', userID)
+
+    return this.http.get<{ words: object }>(url, { params }).pipe(
+      map(response => response.words)
+    );
   }
 
 
